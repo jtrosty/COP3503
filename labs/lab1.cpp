02477a826acb4b624306cdd62f3329bffeb1a7f6
Jonathan Trost <Trost@Jonathans-MBP.fios-router.home>
2021-05-18 22:11:42 -0400

    Completed lab 1 with 30/30 in Zybooks

--- labs/lab1.cpp
+++ labs/lab1.cpp
@@ -6,8 +6,6 @@ using namespace std;
 
 int main()
 {
-   
-   /* Type code here. */
    /* Overall flow of the program:
    Get some input for the title
    Get some input for column header #1
@@ -21,117 +19,120 @@ int main()
       Validate second half of input
       If also valid...
          Store separate pieces
-   
    Print out data in table format
-   
    Print out data in chart format
-   Notes from stff:
-   1. getline(cin, stringVar) - gets all reamaining text on teh current input line. GETLINE DOEST NOT IGNORE LEADING WHITESPACE.
-   2. cin does not go to the next line. must have empty getline with a blank variable to go to the next line.   
-   3. #include <cctype> library, isalpha, isdigit, isspace, tolower, toupper
-   4. find(char or string), substr(index, length), push_back(c), insert(indx, subst), replace (indx, num, substr) CHECK OUT MORE AT C++ STRING LIBRARY!!!!
    */
-    bool run = true;
-    string title;
-    vector<string> colData1(0);
-    vector<int> colData2(0);
-    string userInput;
 
+    // ****************** VARIABLES ****************************************
+    string title = ""; // Title of table
+    string userInput = ""; // Holds user input interation of while loop
+    bool run = true;
+    vector<string> colData1(0); // Holds string data, i of colData1 matched wiht i of colData2
+    vector<int> colData2(0);
+
+    // ***************** TITLE AND HEADERS FOR TABLE AND HISTOGRAM *********
     // Get title
     cout << "Enter a title for the data:" << endl;
     getline(cin, title);
-    cout << "You Entered: " << title << endl;
+    cout << "You entered: " << title << endl;
 
     // Prompt user for headers of 2 coloums
     string colHeader1;
     string colHeader2;
     // Header 1
-    cout << "Enter the colum 1 header: " << endl;
+    cout << "Enter the column 1 header:" << endl;
     getline(cin, colHeader1);
     cout << "You entered: " << colHeader1 << endl;
     // header 2 
-    cout << "Enter the colum 2 header: " << endl;
+    cout << "Enter the column 2 header:" << endl;
     getline(cin, colHeader2);
     cout << "You entered: " << colHeader2 << endl;
 
+
+    // ******************** DATA INPUT WHILE LOOP **************************
+
     while (run) {
-
-        int comma;
-
-        // Test if user input is valid
+        int comma = -1;
+        int intData = 0; 
+        // Test if user inputs commas correctly
         try { 
             cout << "Enter a data point (-1 to stop input):" << endl;
             getline(cin, userInput);
 
             // Break if -1
-            if (userInput == "-1") break;
+            if (userInput == "-1"){
+                run = false;
+                break;   
+            } 
 
             // Check for first Comma
-            if (comma = userInput.find(',') && (userInput.find(',') == string::npos)) {
-                throw runtime_error("No comma.");
+            comma = userInput.find(','); 
+            if ((userInput.find(',') == string::npos)) {
+                throw runtime_error("No comma in string.");
             }
-            cout << "comma " << comma << endl; 
-            // If first comma check good,add string to colData1 
-            colData1.push_back(userInput.substr(0, comma));
 
             // Check if there are too many commas
-            if (userInput.rfind(',') == comma) {
+            if (userInput.rfind(',') != comma) {
                 throw runtime_error("Too many commas in input.");
             }
         }
         catch(runtime_error& e) {
             cout << "Error: " << e.what() << endl;
+            continue;
         }
         catch(invalid_argument& e) {
-                cout << "Error: " << e.what() << endl;
+            cout << "Error: " << e.what() << endl;
+            continue;
         }
-
-        // Attempt to convert the part of string after 1 comma to int.
-        int intData = stoi(userInput.substr(comma, string::npos));
-
-        // If Int succeded, add int data to colData2
-        colData2.push_back(intData);
-        /*
+        
+        // tests if user has integer after the only comma
         try {
             // Attempt to convert the part of string after 1 comma to int.
-            int intData = stoi(userInput.substr(comma, string::npos));
+            intData = stoi(userInput.substr(comma + 1, string::npos)); 
+        } 
+        catch (exception& e) {
+            cout << "Error: Comma not followed by an integer." << endl;
+            continue;
+        }
 
-            // If Int succeded, add int data to colData2
-            colData2.push_back(intData);
-        }
-        catch(invalid_argument& e) {
-            cout << "Error: " << "Comma not followed by an integer." << endl;
-        }
-        */
+        // If first comma check good,add string to colData1 
+        colData1.push_back(userInput.substr(0, comma));
+        // If a good int was pulled, add int data to colData2
+        colData2.push_back(intData);
+        cout << "Data string: " << userInput.substr(0, comma) << endl;
+        cout << "Data integer: " << intData << endl;
     }
 
+    // ****************** DRAW TABLE WITH DATA ***********************************
     // Display data as a table
     // Title
+    cout << endl; // adds space between top of table to data entry section
     cout << setw(33) << right << title << endl;
     // column titles
     cout << setw(20) << left << colHeader1 << "|";
     cout << setw(23) << right << colHeader2 << endl;
     // -------------------------------------
-    cout << setfill('-') << setw(53) << "" << endl;
+    cout << setfill('-') << setw(44) << "" << endl;
     cout << setfill(' '); // reset fill character back to space. 
     // Table Data 
     // colData1 (string) left justified, | colData2 (int) number right justified
     // Breat of The Wild      |               10
     int sizeTable = colData1.size();
-    cout << sizeTable << endl;
     for (int i = 0; i < sizeTable; ++i) {
         cout << setw(20) << left << colData1.at(i) << "|";
         cout << setw(23) << right << colData2.at(i) << endl;
     }
     
+    // ****************** DRAW HISTOGRAM WITH DATA ***********************************
     // Histogram Looks like:
     // colData1 (string) colData2 Int (int # of starts)
     //           String ****************
+    cout << endl; // adds space between top of table to data entry section
     for (int i = 0; i < sizeTable; ++i) {
         cout << setw(20) << right << colData1.at(i) << " ";
         cout << setfill('*') << setw(colData2.at(i)) << "" << endl;
         cout << setfill(' ');
     }
-
+  
     return 0;
 }
