bd16ad924928a22ffc90fd15f6a51419a5d08fa9
Jonathan Trost <Trost@Jonathans-MBP.fios-router.home>
2021-07-18 06:47:38 -0400

    Some work on lab 7, map object does not have emplace function. Not sure why, it looks like C++11 added emplace to the map function.

--- labs/Lab7/main.cpp
+++ labs/Lab7/main.cpp
@@ -3,6 +3,7 @@
 #include <random>
 #include <ctime>
 using namespace std;
+using std::map;
 
 mt19937 random_mt;
 
@@ -10,6 +11,28 @@ int Random(int min, int max)
 {
 	uniform_int_distribution<int> dist(min, max);
 	return dist(random_mt);
+}
+
+void RollDice(int numberOfRolls, int numberOfSides) {
+	map<int, int> rollResults;
+	map<int, int>::iterator iter = rollResults.begin();
+	map<int, string> myMap;
+
+	// Seed the map
+	for (int i = 0; i <= numberOfSides; i++) {
+		rollResults[i] = 0; 
+	}
+
+	for (int i = 0; i < numberOfRolls; i++) {
+		rollResults[Random(1, numberOfSides)]++;
+	}
+
+	for (; iter != rollResults.end(); ++iter) {
+		cout << "key is: " << iter->first;
+		cout << "Value is: " << iter->second;
+	}
+
+	
 }
 
 int main()
@@ -23,6 +46,9 @@ int main()
 	if (option == 1)
 	{
 		int randomSeed;
+		int numOfRolls;
+		int numOfSides;
+
 		cout << "Random seed value: ";
 		cin >> randomSeed;
 		random_mt.seed(randomSeed);
@@ -30,11 +56,14 @@ int main()
 
 		cout << "Number of times to roll the die: ";
       // user input
+	  	cin >> numOfRolls;
 
 		cout << "Number of sides on this die: ";
       // user input
+	  	cin >> numOfSides;
 
 		// Roll Dice
+		void RollDice(int numOfRolls, int numOfSides); 
 	}
 	else if (option == 2)
 	{
