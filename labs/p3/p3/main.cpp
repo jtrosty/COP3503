92a5c3db62b5b3e637d42625127221e2c3a6b234
jtrosty <j.trosty@gmail.com>
2021-07-24 18:58:09 -0400

    Was able to draw tiles correctly, began work on the logic of each individual tile. SOlution right now is the tileInfo struct that constains pos information and a char to record if it is a mine or what number it has or if there is a flag.

--- labs/p3/p3/main.cpp
+++ labs/p3/p3/main.cpp
@@ -9,8 +9,13 @@ using std::endl;
 
 int main()
 {
+    struct TileInfo {
+        int xPos;
+        int yPos;
 
-    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Jon Trost RuleZ");
+        char numOfMinesOrMine; // 1-8 then that is num of mines, or >8 has mine
+    };
+
     sf::Sprite sprite; 
 
     // the cirlce
@@ -23,10 +28,34 @@ int main()
     textureSpriteManager.LoadAllTextures();
 
 
+    // DRAW THE BOARD:
+    // 
     //Config file
     FileLoading fileLoader;
     fileLoader.loadFileHelper("config", fileLoader.config);
 
+    int heightOfMenu = 88;
+    int lengthOfTile = 32;
+
+    int totalWidth = lengthOfTile * fileLoader.configData->column;
+    int totalHeight = (lengthOfTile * fileLoader.configData->rows) + heightOfMenu;
+    int numOfTiles = fileLoader.configData->column * fileLoader.configData->rows;
+    TileInfo* tileInfo = new TileInfo[fileLoader.configData->column * fileLoader.configData->rows];
+    int x = 0;
+    int y = 0;
+    for (int i = 0; i < numOfTiles; i++) {
+        x = i % fileLoader.configData->column;
+        y = i / fileLoader.configData->column;
+        tileInfo->xPos = (x * lengthOfTile);
+        tileInfo->yPos = (y * lengthOfTile);
+        tileInfo->numOfMinesOrMine = 9;
+    }
+
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Jon Trost RuleZ");
+
+
+    cout << "column num " <<  fileLoader.configData->column << endl;
+    
     // Random Test
     int randomX = Random::Int(0, 400);
     cout << randomX << endl;
@@ -47,15 +76,36 @@ int main()
             if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
                 sf::Vector2i mousePos = sf::Mouse::getPosition(window);
                 textureSpriteManager.GetSprite("test_3").setPosition(mousePos.x, mousePos.y);
+                cout << "X is: " << mousePos.x << " Y is: " << mousePos.y << endl;
             }
         }
 
         window.clear();
+
+		sf::Sprite tileSprite = textureSpriteManager.GetSprite("tile_hidden");
+		int xPos = 0;
+		int yPos = 0;
+		for (int i = 0; i < fileLoader.configData->rows; i++) {
+			for (int j = 0; j < fileLoader.configData->column; j++) {
+				tileSprite.setPosition(xPos, yPos);
+				window.draw(tileSprite);
+				xPos += lengthOfTile;
+			}
+            xPos = 0;
+			yPos += lengthOfTile;
+		}
+        sf::Sprite* hidden = &textureSpriteManager.GetSprite("tile_hidden");
+        sf::Sprite* mine = &textureSpriteManager.GetSprite("mine");
+        sf::Sprite* revealed = &textureSpriteManager.GetSprite("tile_revealed");
+		for (int i = 0; i < fileLoader.configData->rows; i++) {
+
+			window.draw(tileSprite);
+		}
+
         window.draw(shape);
         window.draw(sprite);
         window.draw(textureSpriteManager.GetSprite("test_3"));
         window.display();
     }
-
     return 0;
 }
