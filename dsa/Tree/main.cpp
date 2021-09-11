92a39d7aaa99b658f4b9e45739c2c026d4c335e9
jtrosty <j.trosty@gmail.com>
2021-09-11 16:52:45 -0400

    Completed 3 tree problems for DSA, all located in Tree folder.

--- dsa/Tree/main.cpp
+++ dsa/Tree/main.cpp
@@ -2,14 +2,129 @@
 #include <exception>
 #include <vector>
 #include <list>
+#include <string>
 using std::cout;
 using std::endl;
+using std::vector;
 
+long sumVector(std::vector<int> arr); 
+long sumVectorModN(std::vector<int> arr, int n); 
+
+/////////////////////////////////////////////////////////
+// Used by some of the problmes Code given by assignment
 class Node {
     public:
-        int value;
-        Node* next = NULL;
+    int val;
+    vector<Node*> children;
+    // Constructor
+    Node() {}
+    // Constructor with stuff
+    Node(int _val) {
+        val = _val;
+    }
+    // Constructor with children
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
 };
+
+/////////////////////////////////////////////////////////
+// Used by some of the problmes Code given by assignment
+ class TreeNode {
+ public:
+   int val;
+   TreeNode *left;
+   TreeNode *right;
+   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+//////////////////////////////////////////////////////////
+// Preorder problem, bascially, left to right
+void preorder(Node* root) {
+   // your code 
+   cout << root->val << " ";
+   for (int i = 0; i < root->children.size(); i++) {
+       preorder(root->children.at(i));
+   }
+}
+
+////////////////////////////////////////////////////////
+// Traversing a tree by level the followign 2 functions are together
+void levelOrder(vector<Node*>& level, int depth) {
+    // End condition
+    if (level.size() == 0) return;
+
+    // Common Case
+    vector<Node*> nextLevel;
+    cout << "Level: " << depth << endl;
+    for (int i = 0; i < level.size(); i++) {
+        cout << level.at(i)->val << " ";
+        for (int j = 0; j < level.at(i)->children.size(); j++) {
+            nextLevel.push_back(level.at(i)->children.at(j));
+        }
+    }
+    cout << endl;
+    levelOrder(nextLevel, depth + 1);
+}
+
+void levelOrder(Node* root) {
+    // Handles only the first level, the root
+    vector<Node*> level;
+    cout << "Level: 1" << endl;
+    cout << root->val << " " << endl;
+    for (int i = 0; i < root->children.size(); i++) {
+        level.push_back(root->children.at(i));
+    }
+    levelOrder(level, 2);
+}
+
+/////////////////////////////////////////////////////////////
+// Binary Tree Inorder Traversal
+
+std::string traverse(TreeNode* root) {
+    //your code here
+    std::string result;
+    if (root == nullptr) return "";
+    result += traverse(root->left);
+    result += std::to_string(root->val); 
+    result += traverse(root->right);
+    return result;
+}
+
+
+int main (void) {
+    // Setup
+    Node* root = new Node(12);
+    Node* root1 = new Node(1);
+    Node* root2 = new Node(4);
+    Node* root3 = new Node(5);
+    Node* root4 = new Node(7);
+    Node* root5 = new Node(11);
+    Node* root6 = new Node(8);
+    root->children.push_back(root1);
+    root->children.push_back(root2);
+    root->children.push_back(root3);
+    root2->children.push_back(root4);
+    root2->children.push_back(root5);
+    root3->children.push_back(root6);
+
+    TreeNode* leftNode = new TreeNode(1);
+    TreeNode* rightNode = new TreeNode(3);
+    TreeNode* start = new TreeNode(2, leftNode, rightNode);
+    
+    preorder(root);
+    cout << endl;
+    levelOrder(root);
+    cout << endl;
+    cout << traverse(start);
+
+
+
+    return 0;
+}
 
 
 long sumVector(std::vector<int> arr) {
@@ -24,12 +139,3 @@ long sumVectorModN(std::vector<int> arr, int n) {
     if (n <= 0) throw std::logic_error("Modular arithmetic undefined for non-positive integers!");
     return sumVector(arr) % n;
 }
-
-/*
-
-int main (void) {
-    cout << "it works" << endl;
-    return 0;
-}
-
-*/
