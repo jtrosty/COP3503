bbfcead854ae6acc1b0aa19d4985c61727211145
jtrosty <j.trosty@gmail.com>
2021-09-11 05:01:47 -0400

    Added teh catch 2 testign code to the tree folder. Worked to get familiar with how to perform unit testing

--- dsa/Tree/main.cpp
+++ dsa/Tree/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <exception>
+#include <vector>
 #include <list>
 using std::cout;
 using std::endl;
@@ -9,11 +11,25 @@ class Node {
         Node* next = NULL;
 };
 
-float interQuartile(Node* head) {
 
+long sumVector(std::vector<int> arr) {
+    long acc = 0;
+    for(int i = 0; i < arr.size(); i++) {
+        acc += arr.at(i);
+    }
+    return acc;
 }
+
+long sumVectorModN(std::vector<int> arr, int n) {
+    if (n <= 0) throw std::logic_error("Modular arithmetic undefined for non-positive integers!");
+    return sumVector(arr) % n;
+}
+
+/*
 
 int main (void) {
     cout << "it works" << endl;
     return 0;
 }
+
+*/
