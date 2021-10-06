f1b49b320acfc31803852150dd9a0e1b3f09129f
Jonathan Trost <Jonathan Trost>
2021-10-05 22:05:20 -0400

    Began work on Heap 6.1.1

--- dsa/heaps/main.cpp
+++ dsa/heaps/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-using namespace std;
+#include <sstream>
+//using namespace std;
 
 int* extractMax(int arr[], int size) {
     //code here
@@ -28,9 +29,64 @@ int* extractMax(int arr[], int size) {
     return arr;
 }
 
+//////////////////////////////////////////////////////////////////////
+//                   Heap 6.1.1
+
+int readHeap(int* theheap)
+{
+    //your code here
+    //use std::cin to read in the data
+    //return the size of the heap   
+    std::string input;
+    int counter = 0;
+    for ( ; input != "-1" ; getline(std::cin, input, ' ')) {
+        theheap[counter] = std::stoi(input);
+    }
+    return ++counter;
+}
+
+void heapRemove(int* theheap, int& size)
+{
+   //your code here  
+   // 'Delete' the first nod,e reduce the size of the array.
+   theheap[0] = theheap[size--];
+   int root = 0;
+   int leftChild = 1;
+   int rightChild = 2;
+   int temp = 0;
+
+   while(theheap[root] > theheap[leftChild] || theheap[root] > theheap[rightChild]) {
+       if (theheap[leftChild] < theheap[rightChild] && leftChild < size) {
+           temp = theheap[leftChild];
+           theheap[leftChild] = theheap[root];
+           theheap[root] = temp;
+           root = leftChild;
+       }
+       if (theheap[rightChild] < theheap[leftChild] && rightChild < size) {
+           temp = theheap[rightChild];
+           theheap[rightChild] = theheap[root];
+           theheap[root] = temp;
+           root = rightChild;
+       }
+        leftChild = (2 * root) + 1;
+        rightChild = (2 * root) + 2;
+    }
+}
+
+void heapPrint(int* theheap, int size)
+{
+    //use cout to print the array representing the heap
+    for (int i = 0; i < size; i++) {
+        std::cout << theheap[i] << " ";
+    }
+}
+
 //Don't change main
 int main() {
 
+////////////////////////////////////////////
+// HEAP Quiz
+/*
    int size;
    cin >> size;
    int* arr = new int[size];
@@ -43,4 +99,5 @@ int main() {
       cout << arr[i] << " ";
 
    return 0;
+   */
 }
