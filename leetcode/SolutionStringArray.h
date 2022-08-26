5a61c8952b23cced4de97c9fb9f52116259494ae
Jon Trost <j.trosty@gmail.com>
2022-08-26 06:39:21 -0400

    learned the fast way to rotate a matrix.

--- leetcode/SolutionStringArray.h
+++ leetcode/SolutionStringArray.h
@@ -22,7 +22,49 @@ using std::make_pair;
 using std::make_tuple;
 using std::get;
 using std::tie;
+using std::swap;
 
+class SolutionRotateFast {
+public:
+/*
+    void swap(vector<vector<int>>& matrix, int i, int j, int l) {
+        int TL = matrix.at(i).at(j);
+        int TR = matrix.at(j).at(l - i);
+        int BR = matrix.at(l - i).at(l - j);
+        int BL = matrix.at(l - j).at(i);
+
+        int temp;
+        temp = matrix.at(i).at(j);
+        //  TL                      BL
+        matrix.at(i).at(j) = matrix.at(l - j).at(i);
+        //    BL                  BR
+        matrix.at(l - j).at(i) = matrix.at(l - i).at(l - j) ;
+        //    BR                  TR
+        matrix.at(l - i).at(l - j) = matrix.at(j).at(l - i);
+        //    TR               TL
+        matrix.at(j).at(l - i) = temp;
+    }
+    */
+
+    void rotate(vector<vector<int>>& matrix) {
+        int matrixLength = matrix.size();
+        int oddSizeAdjustment = 0;
+        int half = matrixLength / 2;
+
+        // transpose
+        for (int i = 0; i < matrixLength; i++) {
+            for (int j = i; j < matrixLength; j++) {
+                swap(matrix.at(i).at(j), matrix.at(j).at(i));
+            }
+        }
+        // reverse
+        for (int i = 0; i < matrixLength; i++) {
+            for (int j = i; j < half; j++) {
+                swap(matrix.at(i).at(j), matrix.at(i).at(matrixLength - i - 1));
+            }
+        }
+    }
+};
 class SolutionRotate {
 public:
     void swap(vector<vector<int>>& matrix, int i, int j, int l) {
