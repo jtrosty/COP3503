a5ee334c29ab28a7afa115222fb2924426813f82
Jon Trost <j.trosty@gmail.com>
2022-09-04 06:51:25 -0400

    Got catch 2 working with othello

--- leetcode/Othello/othello.h
+++ leetcode/Othello/othello.h
@@ -1,4 +1,4 @@
-#include<vector>
+#include <vector>
 
 using std::vector;
 
@@ -10,22 +10,33 @@ struct Tile {
 
 // 2,2, We know that there is a enemy piece at 2,3, We need to lookk at 2,5
 // 2,2 and enemyat 3,3. We need look donw to 8,8
-int checkTiles(int x, int y, int directionX, int directionY, char team, char enemyTile, vector<vector<Tile>>& board) {
+int checkTiles(int x, int y, int directionX, int directionY, char team, char enemyTile, vector<vector<int>>& board) {
     int boardSize = board.size() - 1;
-    int result = 0;
-    int count = 0;
-    Tile* nextTile = &(board.at(x).at(y));
+    int furtherTiles = 0;
   
   	if (x >= 0 && x <= boardSize && y >= 0 && y <= boardSize) {
         x += directionX;
         y += directionY;
     }
+    int tile = board.at(x).at(y);
+    if (tile == enemyTile) {
+      furtherTiles = checkTiles(x, y, directionX, directionY, team, enemyTile, board);
+      if (furtherTiles < 0) {
+        return -1;
+      }
+      else {
+        return 1 + furtherTiles;
+      }
+    }
+    else {
+      return -1;
+    }
       
+      /*
   	while (nextTile->occupied != 0 &&
             x >= 0 && x <= boardSize && y >= 0 && y <= boardSize) {
         x += directionX;
         y += directionY;
-      	Tile* nextTile = &(board.at(x).at(y));
         if (nextTile->occupied == enemyTile) {
             count++;
         }
@@ -39,11 +50,11 @@ int checkTiles(int x, int y, int directionX, int directionY, char team, char ene
         }
     }
   	return result;
+    */
 }
 
-int howManyPiecesFlip(char team, int x, int y, vector<vector<Tile>>& board) {
-  	int numOfneighbors = board.at(x).at(y).neighbors->size();
-  	Tile* currentTile = &(board.at(x).at(y));
+int howManyPiecesFlip(char team, int x, int y, vector<vector<int>>& board) {
+  	int* currentTile = &(board.at(x).at(y));
   	char enemyTile;
     int result = 0;
   
@@ -53,7 +64,7 @@ int howManyPiecesFlip(char team, int x, int y, vector<vector<Tile>>& board) {
   	// At the edge or not. 
     for (int i = -1; i <= 1; i++) {
         for (int j = -1; j <= 1; j++) {
-            result += checkTiles(x, y, i, j, enemyTile, board); 
+            result += checkTiles(x, y, i, j, team, enemyTile, board); 
         }
     }
     return result;
--- leetcode/Othello/test/test.cpp
+++ leetcode/Othello/test/test.cpp
@@ -1,32 +1,18 @@
-#include "../othello.cpp"
+
+#include "../othello.h"
 
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-TEST_CASE("1 insert works, unable to insert same", "[insert]"){
-    TreeNode treeForTest;
-    REQUIRE(treeForTest.insert("Jon", 1234));
-    REQUIRE_FALSE(treeForTest.insert("Jon", 1234));
-}
+TEST_CASE("Test case for Othello", "[insert]"){
+    vector<vector<int>> testBoard {     {0, 0, 0, 0, 0, 0, 0, 0},
+                                        {0, 0, 0, 0, 0, 0, 0, 0},   
+                                        {0, 0, 0, 0, 0, 0, 0, 0},
+                                        {0, 0, 0, 0, 0, 0, 0, 0},
+                                        {0, 0, 0, 0, 0, 0, 0, 0},
+                                        {0, 0, 0, 0, 0, 0, 0, 0},
+                                        {0, 0, 0, 0, 0, 0, 0, 0},
+                                        {0, 0, 0, 0, 0, 0, 0, 0} };
 
-TEST_CASE("2 Right Balance of 3 nodes", "[insert]") {
-    TreeNode treeForTest;
-    treeForTest.insert("small", 1);
-    treeForTest.insert("middle", 2);
-    treeForTest.insert("big", 3);
-    REQUIRE(treeForTest.root->left->name == "small");
-    REQUIRE(treeForTest.root->name == "middle");
-    REQUIRE(treeForTest.root->right->name == "big");
-    REQUIRE(treeForTest.testTreeBalance());
-}
-
-TEST_CASE("3 Left Balance of 3 nodes", "[insert]") {
-    TreeNode treeForTest;
-    treeForTest.insert("big", 3);
-    treeForTest.insert("middle", 2);
-    treeForTest.insert("small", 1);
-    REQUIRE(treeForTest.root->left->name == "small");
-    REQUIRE(treeForTest.root->name == "middle");
-    REQUIRE(treeForTest.root->right->name == "big");
-    REQUIRE(treeForTest.testTreeBalance());
+    REQUIRE(howManyPiecesFlip(1, 0, 0, testBoard) == 0);
 }
