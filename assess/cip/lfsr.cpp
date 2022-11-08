d6bb8b70c36a661fc6d6fbaf431dea167fc3dc67
Jon Trost <j.trosty@gmail.com>
2022-11-07 20:35:15 -0500

    Added shift right for a whole array and XOR for a whole array

--- assess/cip/lfsr.cpp
+++ assess/cip/lfsr.cpp
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 
 unsigned char *testLSFR(unsigned char *data, int dataLength, unsigned int initialValue);
+void shiftRight(unsigned char *data, int dataLength); 
+void XORarray(unsigned char *data, int dataLength);
 
 int main(void) {
     unsigned char *data;
@@ -16,11 +18,34 @@ int main(void) {
     return 0;
 }
 
+void shiftRight(unsigned char *data, int dataLength)  {
+    unsigned char current = 0x00;
+    unsigned char previous = 0x00;
+    // Shift
+    for (int i = 0; i < dataLength; i++) {
+        previous = current;
+        current = (data[i] & 0x01) << 7;
+        data[i] = data[i] >> 1;
+        if (i == 0) {
+            
+        }
+        else {
+            data[i] = data[i] | previous;
+        }
+    }
+}
+
+void XORarray(unsigned char *data, int dataLength, unsigned int feedback)  { 
+    int *dataInt = (int*)data;
+    *dataInt = (*dataInt) ^ feedback;
+}
 
 unsigned char *testLSFR(unsigned char *data, int dataLength, unsigned int initialValue) {
-    char lowestBit = *data & 0x01;
-    for (int i = 0; i < 8; i++) {
-
+    unsigned int feedback = 0x87654321;
+    char lowestBit = data[dataLength - 1] & 0x01;
+    for (int i = 0; i < 32; i++) {
+        shiftRight(data, dataLength);
+        XORarray(data, dataLength, feedback);
     }
 
     return data;
