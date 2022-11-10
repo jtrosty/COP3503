b9ec1ddc507f099bd2f512836df6d7456892caf6
Jon Trost <j.trosty@gmail.com>
2022-11-09 19:15:45 -0500

    Complete first part of take home hope for cip.

--- assess/cip/lfsr.cpp
+++ assess/cip/lfsr.cpp
@@ -1,23 +1,49 @@
 #include <stdlib.h>
 
 unsigned char *testLSFR(unsigned char *data, int dataLength, unsigned int initialValue);
+unsigned char *LSFR(unsigned char *data, int dataLength, unsigned int initialValue); 
 void shiftRight(unsigned char *data, int dataLength); 
 void XORarray(unsigned char *data, int dataLength);
 
 int main(void) {
     unsigned char *data;
-    int size = 4;
+    int size = 5;
     data = (unsigned char*)malloc(size);
-    for (int i = 0; i < size; i++) {
-        data[i] = 0xFF;
-    } 
+    data[0] = 'a';
+    data[1] = 'p';
+    data[2] = 'p';
+    data[3] = 'l';
+    data[4] = 'e';
     unsigned int feedback = 0x87654321;
-    unsigned int initial = 0xFFFFFFFF;
-    testLSFR(data, size, feedback);
-    testLSFR(data, size, initial);
+    unsigned int initial = 0x12345678;
+    //testLSFR(data, size, feedback);
+    //testLSFR(data, size, initial);
+    LSFR(data, size, initial);
+    LSFR(data, size, initial);
 
 
     return 0;
+}
+
+unsigned char *LSFR(unsigned char *data, int dataLength, unsigned int initialValue) {
+    unsigned int feedback = 0x87654321;
+    char lowestBit = 0x00;
+    int bitsInByte = 8;
+
+    for (int j = 0; j < dataLength; j++) {
+        char keyByte = 0;
+        for (int i = 0; i < bitsInByte; i++) {
+            lowestBit = initialValue & 0x00000001;
+            initialValue = initialValue >> 1;
+            if (lowestBit == 1)  {
+                initialValue = initialValue ^ feedback;
+            }
+            keyByte = initialValue & 0x000000FF;
+        }
+        //data[dataLength - j - 1] = data[dataLength - j - 1] ^ keyByte;
+        data[j] = data[j] ^ keyByte;
+    }
+    return data;
 }
 
 void shiftRight(unsigned char *data, int dataLength)  {
