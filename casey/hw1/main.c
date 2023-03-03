e1f9ee27b964d15e4de6719c10fd4003cd3169fd
Jon Trost <j.trosty@gmail.com>
2023-03-03 07:58:54 -0500

    Continued work on hw for performance aware.

--- casey/hw1/main.c
+++ casey/hw1/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 long getfileSize(char* filename) {
     FILE* fp = fopen(filename, "r");
@@ -13,6 +14,24 @@ long getfileSize(char* filename) {
     long length = ftell(fp);
     fclose(fp);
     return length;
+}
+
+
+void decode(uint16_t fullInst) {
+    const uint8_t const movRtoR = 0x00100010;
+
+    printf("%X \n", fullInst);
+    uint8_t opcode = (fullInst > 10) ; // need the first 6 bits
+    switch (opcode) {
+        case 0x00100010: {
+            mov(fullInst);
+            break;
+        } 
+    }
+}
+
+void mov(uint16_t fullInst) {
+    printf("mov ");
 }
 
 int main(int argc, char* argv[]) {
@@ -34,8 +53,12 @@ int main(int argc, char* argv[]) {
         if (fileSize == 0) {
             perror("File filed to open.");
         }
-        size_t bytes = sizeof(char) * (int)fileSize;
-        rawInstructionsData = malloc(bytes);
+        size_t bytes = sizeof(char) * ((int)fileSize);
+        //rawInstructionsData = (char*)malloc(bytes);
+        rawInstructionsData = (char*)malloc(bytes * sizeof(char));
+        if (!rawInstructionsData) {
+            perror("Failed to get memory.");
+        }
         fp = fopen(debug_fileName, "r");
     }
     else {
@@ -47,9 +70,26 @@ int main(int argc, char* argv[]) {
     int c;
     for (long i = 0; i < fileSize; i++) {
         c = fgetc(fp);
-        char temp = (char)(c | 0x000000FF);
+        if (feof(fp)) {
+            break;
+        }
+        char temp = (char)(c & 0x000000FF);
         rawInstructionsData[i] = temp;
     }
+    fclose(fp);
 
+    if (fileSize % 2 ==1) {
+        perror("odd number of bytes in instructions.");
+    }
+
+    long numOfInstructiosn = fileSize / 2;
+    uint16_t* inst = (uint16_t*)rawInstructionsData;
+    for (long i = 0; i < numOfInstructiosn; i++) {
+        decode(inst[i]);
+    }
+
+
+    free(rawInstructionsData);
+    printf("you did it. \n");
     return 0;
 }
